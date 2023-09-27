c5ecfddb4bdf4a7340735b825122222ddf5f9421
sjsimpson <spencerjsimpson@gmail.com>
2023-09-27 10:00:29 -0600

    feat: update typewriter to stop blinking when typing

--- src/app/_components/typewriter.tsx
+++ src/app/_components/typewriter.tsx
@@ -1,13 +1,75 @@
 "use client";
 
-import useTypewriter from "@/lib/useTypewriter";
+import { useState, useEffect } from "react";
 
 export default function Typewriter({ prompts }: { prompts: string[] }) {
-  const typewriter = useTypewriter(prompts);
+  const [paused, setPaused] = useState(false);
+  const [typing, setTyping] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [textIndex, setTextIndex] = useState(0);
+  const [text, setText] = useState("");
+
+  const typingSpeed = 100;
+  const clearSpeed = 20;
+  const pauseDuration = 2000;
+
+  useEffect(() => {
+    function addNextLetter() {
+      setText(text + prompts[index][textIndex]);
+      setTextIndex(textIndex + 1);
+    }
+
+    function removeLastLetter() {
+      setText(text.slice(0, textIndex - 1));
+      setTextIndex(textIndex - 1);
+    }
+
+    function pause() {
+      setPaused(true);
+      setTyping(false);
+      setTimeout(() => setPaused(false), pauseDuration);
+    }
+
+    function nextWord() {
+      index < prompts.length - 1 ? setIndex(index + 1) : setIndex(0);
+      setTyping(true);
+    }
+
+    let timeout: any;
+    if (typing) {
+      if (textIndex < prompts[index].length) {
+        timeout = setTimeout(addNextLetter, typingSpeed);
+      } else {
+        pause();
+      }
+    } else if (!paused) {
+      if (textIndex > 0) {
+        timeout = setTimeout(removeLastLetter, clearSpeed);
+      } else {
+        nextWord();
+      }
+    }
+
+    return () => clearTimeout(timeout);
+  }, [
+    text,
+    typing,
+    index,
+    textIndex,
+    paused,
+    prompts,
+    clearSpeed,
+    typingSpeed,
+    pauseDuration,
+  ]);
 
   return (
-    <span className="after:ml-1 after:h-full after:w-2 after:border after:border-black after:dark:border-white after:animate-blink">
-      {typewriter}
+    <span
+      className={`after:ml-1 after:h-full after:w-2 after:border after:border-black after:dark:border-white ${
+        !typing ? "after:animate-blink" : ""
+      }`}
+    >
+      {text}
     </span>
   );
 }
